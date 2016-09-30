05703d8aea8237d9dbab3b946cf5ca4fb831fb7a
Luca Realdi <lr@opencontent.it>
2016-09-30 18:26:26 +0200

    fix workflow

--- eventtypes/event/moderaeventi/moderaeventitype.php
+++ eventtypes/event/moderaeventi/moderaeventitype.php
@@ -1,37 +1,36 @@
 <?php
+
 class moderaEventiType extends eZWorkflowEventType
 {
-	const TYPE_ID = 'moderaeventi';
+    const TYPE_ID = 'moderaeventi';
     private $currentUser;
 
-	public function __construct()
+    public function __construct()
     {
-		$this->eZWorkflowEventType( self::TYPE_ID, 'Modera Eventi' );
-	}
+        $this->eZWorkflowEventType(self::TYPE_ID, 'Modera Eventi');
+    }
 
-	public function execute( $process, $event )
+    public function execute($process, $event)
     {
-		$processParams = $process->attribute( 'parameter_list' );
-		$trigger = $processParams['trigger_name'];
-		if ( $trigger == 'post_publish' )
-        {
-			$objectID = $processParams['object_id'];
-			$contentObject = eZContentObject::fetch( $objectID );
-			if( $contentObject instanceof eZContentObject )
-			{
-				$helper = new OCModeraEventiHelper( $contentObject );                
-                if ( $helper->needModeration() )
-                {                    
-                    $helper->makeInModerationQueue();
+        $processParams = $process->attribute('parameter_list');
+        $trigger = $processParams['trigger_name'];
+        if ($trigger == 'post_publish') {
+            $objectID = $processParams['object_id'];
+            $contentObject = eZContentObject::fetch($objectID);
+            if ($contentObject instanceof eZContentObject) {
+                $helper = new OCModeraEventiHelper($contentObject);
+                if ($helper->needWorkflow()) {
+                    if ($helper->needModeration()) {
+                        $helper->makeInModerationQueue();
+                    } else {
+                        $helper->addLocations();
+                    }
                 }
-                else
-                {
-                    $helper->addLocations();
-                }
-			}
+            }
         }
+
         return eZWorkflowType::STATUS_ACCEPTED;
-	}
+    }
 }
 
-eZWorkflowEventType::registerEventType( moderaEventiType::TYPE_ID, 'moderaEventiType' );
+eZWorkflowEventType::registerEventType(moderaEventiType::TYPE_ID, 'moderaEventiType');
